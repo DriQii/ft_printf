47a4b4449febb74d9446b484052e436fa7646845
Enzo Vella <evella@c2r3p9.42perpignan.fr>
2023-10-05 20:51:38 +0200

    setup

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -1,6 +1,20 @@
 #include <libft.h>
+#include "ft_printf.h"
+
+int ft_printf(const char *s, ...)
+{
+    va_list args;
+    char *str;
+
+    va_start(args, s);
+    str = va_arg(args, char *);
+    printf("%s\n", s);
+    printf("%s\n", str);
+
+    return (1);
+}
 
 int main(void)
 {
-    ft_putnbr(45);
+    ft_printf("salut", "ca va fdzfgs");
 }
